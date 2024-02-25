2d172060ae88b1313cb672b7d0f7d2f016842345
Alan Pereira <alan_pereira@mckinsey.com>
2024-02-25 15:17:42 +0000

    feat: add github page

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import {packageJson} from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,6 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: '/MySelf/'
+  //MySelf Github Pages
+  base: process.env.PREVIEW === 'true' ? '/' : '/MySelf/',
 });
